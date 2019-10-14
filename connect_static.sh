69986cad63382717cf662598dd122d62a0b4f859
puppy <puppy@???.com>
2019-10-14 17:40:37 +0800

    updating resolv.conf

--- connect_static.sh
+++ connect_static.sh
@@ -4,6 +4,7 @@ device="wlan0"
 ip_addr="192.168.0.201"
 def_gw="192.168.0.1"
 dns_server="1.1.1.1"
+dns_resolv_conf="/etc/resolv.conf"
 echo "removing all previous wpa_supplicant deamons"
 killall wpa_supplicant
 
@@ -14,4 +15,4 @@ echo "[DEBUG] connection should be established"
 ip addr add ${ip_addr}/24 dev ${device}
 # hardcoding the address
 route add default gw ${def_gw} ${device}
-echo "nameserver ${dns_server}"
+echo "nameserver ${dns_server}" > ${dns_resolv_conf}
